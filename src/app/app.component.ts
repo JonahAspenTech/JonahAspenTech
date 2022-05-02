3316fd5763091c90a99bc6f34c6daf00e67f71ee
Jonah <Jonah.Wilson@aspentech.com>
2022-05-02 14:27:44 -0400

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
@@ -8,9 +8,7 @@ import {Router} from '@angular/router';
 })
 export class AppComponent {
   title = 'my-app';
-  constructor(private router: Router) { }
 
-  goToShellAndTube(message: String){
-    this.router.navigate(['/shellandtube'], {queryParams:{message: message}});
-  }
+
+ 
 }
