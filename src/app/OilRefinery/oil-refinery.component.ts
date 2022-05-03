9e5330f4ef13e4a049ddc666f5275f34033ced66
Jonah <Jonah.Wilson@aspentech.com>
2022-05-03 15:06:04 -0400

    commit

--- src/app/OilRefinery/oil-refinery.component.ts
+++ src/app/OilRefinery/oil-refinery.component.ts
@@ -19,31 +19,31 @@ export class ShellAndTubeComponent implements OnInit {
   }
 
   goToCrudeOilDistillation(message: String){
-    this.router.navigate(['/crude-oil-refinery/crude-oil-distillation']);
+    this.router.navigate(['/crude-oil-refinery/crude-oil-distillation'], {queryParams: {message: message}});
   }
 
   goToDieselHydroTreating(message: String){
-    this.router.navigate(['/crude-oil-refinery/diesel-hydro-treating']);
+    this.router.navigate(['/crude-oil-refinery/diesel-hydro-treating'], {queryParams: {message: message}});
   }
  
   goToFluidCatalyticCracking(message: String){
-    this.router.navigate(['/crude-oil-refinery/fluid-catalytic-cracking']);
+    this.router.navigate(['/crude-oil-refinery/fluid-catalytic-cracking'], {queryParams: {message: message}});
   }
 
   goToIsomerization(message: String){
-    this.router.navigate(['/crude-oil-refinery/isomerization']);
+    this.router.navigate(['/crude-oil-refinery/isomerization'], {queryParams: {message: message}});
   }
 
   goToSemiRegenerativeReformer(message: String){
-    this.router.navigate(['/crude-oil-refinery/semi-regenerative-reformer']);
+    this.router.navigate(['/crude-oil-refinery/semi-regenerative-reformer'], {queryParams: {message: message}});
   }
 
   goToSulferRecovery(message: String){
-    this.router.navigate(['/crude-oil-refinery/sulfer-recovery-unit']);
+    this.router.navigate(['/crude-oil-refinery/sulfer-recovery-unit'], {queryParams: {message: message}});
   }
 
   goToVacuumDisillation(message: String){
-    this.router.navigate(['/crude-oil-refinery/vacuum-distillation']);
+    this.router.navigate(['/crude-oil-refinery/vacuum-distillation'], {queryParams: {message: message}});
   }
 
 }
